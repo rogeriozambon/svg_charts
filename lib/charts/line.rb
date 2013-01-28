c2d789544f84dbe7afc549e63c707d7648f36584
Rogério Zambon <rogeriozambon@gmail.com>
2013-01-28 11:32:49 -0200

    Separate elements

--- lib/charts/line.rb
+++ lib/charts/line.rb
@@ -3,30 +3,19 @@ module SVGCharts
     include SVGElements
 
     def initialize(options = {})
-      raise ArgumentError if options.empty?
-
-      [:width, :height, :y_retreat, :y_label, :x_retreat, :x_label, :x_scale, :data, :data_color].each do |key|
-        raise ArgumentError unless options.has_key?(key)
-      end
+      validation options, [:width, :height, :y_retreat, :x_retreat]
 
       @width = options[:width]
       @height = options[:height]
 
       @y_retreat = options[:y_retreat]
-      @y_label = options[:y_label]
-
       @x_retreat = options[:x_retreat]
-      @x_label = options[:x_label]
-      @x_scale = options[:x_scale]
-
-      @data = options[:data]
-      @data_color = options[:data_color]
     end
 
-    def draw_scale
-      scales = ""
+    def scale(options = {})
+      validation options, [:y_label, :x_label]
 
-      scales << line({
+      scale = line({
         :x1 => @x_retreat - 8,
         :x2 => @x_retreat - 8,
         :y1 => @y_retreat - 2,
@@ -35,7 +24,7 @@ module SVGCharts
         :color => "#aaa"
       })
 
-      scales << line({
+      scale << line({
         :x1 => 0,
         :x2 => @width,
         :y1 => @height,
@@ -44,30 +33,32 @@ module SVGCharts
         :color => "#aaa"
       })
 
-      scales << text({
-        :x => @width/2 - (@y_label.length * 2),
+      scale << text({
+        :x => @width/2 - (options[:y_label].length * 2),
         :y => @height + 30,
         :color => "#bbb",
         :font_size => 11,
         :font_weight => "bold",
-        :value => @x_label
+        :value => options[:x_label]
       })
 
-      scales << text_with_rotation({
-        :x => @height/2 - (@y_label.length * 2),
+      scale << text_with_rotation({
+        :x => @height/2 - (options[:y_label].length * 2),
         :y => 8,
         :color => "#bbb",
         :font_size => 11,
         :font_weight => "bold",
         :rotate => "270 #{@height/2} #{@height/2}",
-        :value => @y_label
+        :value => options[:y_label]
       })
 
-      scales
+      scale
     end
 
-    def draw_chart
-      calculate_coordinates
+    def draw(options = {})
+      validation options, [:scale, :data, :data_color, :show_scale, :show_dashed]
+
+      calculate_coordinates options[:data]
 
       elements = ""
 
@@ -81,14 +72,16 @@ module SVGCharts
           :color => "#999"
         })
 
-        elements << line_dashed({
-          :x1 => coordinate[0],
-          :x2 => coordinate[0],
-          :y1 => @height,
-          :y2 => coordinate[2],
-          :line_width => 1,
-          :color => "#b9b9b9"
-        })
+        if options[:show_dashed]
+          elements << line_dashed({
+            :x1 => coordinate[0],
+            :x2 => coordinate[0],
+            :y1 => @height,
+            :y2 => coordinate[2],
+            :line_width => 1,
+            :color => "#b9b9b9"
+          })
+        end
 
         elements << circle({
           :x => coordinate[0],
@@ -102,45 +95,55 @@ module SVGCharts
         elements << text({
           :x => coordinate[0] - 10,
           :y => coordinate[2] - 10,
-          :color => @data_color,
+          :color => options[:data_color],
           :font_size => 11,
           :font_weight => "bold",
-          :value => @data[i]
+          :value => options[:data][i]
         })
 
-        elements << text({
-          :x => coordinate[0] - 5,
-          :y => @height + 12,
-          :color => "#999",
-          :font_size => 10,
-          :font_weight => "normal",
-          :value => @x_scale[i]
-        })
+        if options[:show_scale]
+          elements << text({
+            :x => coordinate[0] - 5,
+            :y => @height + 12,
+            :color => "#999",
+            :font_size => 10,
+            :font_weight => "normal",
+            :value => options[:scale][i]
+          })
+        end
       end
 
       elements
     end
 
     private
-    def calculate_coordinates
+    def calculate_coordinates(data)
       @coordinates = []
 
-      y_positions = (@height - @y_retreat)/@data.max
-      x_positions = @width/@data.size
+      y_positions = (@height - @y_retreat)/data.max
+      x_positions = @width/data.size
 
-      @data.each_with_index do |value, i|
+      data.each_with_index do |value, i|
         x1 = @x_retreat + (x_positions * i)
         y1 = @height - (y_positions * value)
 
-        unless @data[i + 1].nil?
+        unless data[i + 1].nil?
           x2 = @x_retreat + (x_positions * (i + 1))
-          y2 = @height - (y_positions * @data[i + 1])
+          y2 = @height - (y_positions * data[i + 1])
         else
           x2 = x1
           y2 = y1
         end
 
         @coordinates << [x1.abs, x2.abs, y1.abs, y2.abs]
+      end
+    end
+
+    def validation(options, params)
+      raise ArgumentError if options.empty?
+
+      params.each do |key|
+        raise ArgumentError unless options.has_key?(key)
       end
     end
   end
--- spec/charts/line_spec.rb
+++ spec/charts/line_spec.rb
@@ -1,79 +1,135 @@
 require "spec_helper"
 
 describe SVGCharts::Line do
-  context "missing parameters" do
-    it "when hash empty" do
-      expect { chart = SVGCharts::Line.new }.to raise_error(ArgumentError)
-    end
+  context "initialize object" do
+    context "missing parameters" do
+      it "when hash empty" do
+        expect { chart = SVGCharts::Line.new }.to raise_error(ArgumentError)
+      end
 
-    it "when the hash keys is incomplete" do
-      options = {
-        :width => 300,
-        :height => 300
-      }
+      it "when the hash keys is incomplete" do
+        options = {
+          :width => 300,
+          :height => 300
+        }
 
-      expect { chart = SVGCharts::Line.new options }.to raise_error(ArgumentError)
+        expect { chart = SVGCharts::Line.new options }.to raise_error(ArgumentError)
+      end
     end
   end
 
-  context "when generated" do
+  def count_words(expression, target)
+    counter = 0
+
+    expression.split.each do |word|
+      counter += 1 if word.include? target
+    end
+
+    counter
+  end
+
+  context "set scale" do
     before do
-      @options = {
-        :height => 320,
-        :width => 680,
+      @svg_chart = SVGCharts::Line.new({
+        :height => 300,
+        :width => 300,
         :y_retreat => 20,
-        :y_label => "Numbers",
-        :x_retreat => 20,
-        :x_label => "Letters",
-        :x_scale => ["A", "B", "C"],
-        :data => [10, 20, 30],
-        :data_color => "#000000"
-      }
+        :x_retreat => 20
+      })
     end
 
-    it "check chart values" do
-      line_chart = SVGCharts::Line.new(@options).draw_chart
-
-      values = line_chart.gsub( /<[^>]*>/, " ").split
-      values.should include("10")
-      values.should include("20")
-      values.should include("30")
-      values.should include("A")
-      values.should include("B")
-      values.should include("C")
-    end
-
-    it "check scale values" do
-      line_chart = SVGCharts::Line.new(@options).draw_scale
-
-      values = line_chart.gsub( /<[^>]*>/, " ").split
-      values.should include("Letters")
-      values.should include("Numbers")
-    end
-
-    def count_words(expression, target)
-      counter = 0
-
-      expression.split.each do |word|
-        counter += 1 if word.include? target
+    context "missing parameters" do
+      it "when hash empty" do
+        expect { chart = @svg_chart.scale }.to raise_error(ArgumentError)
       end
 
-      counter
+      it "when the hash keys is incomplete" do
+        options = {
+          :y_label => "First label"
+        }
+
+        expect { chart = @svg_chart.scale options }.to raise_error(ArgumentError)
+      end
     end
 
-    it "check chart elements" do
-      line_chart = SVGCharts::Line.new(@options).draw_chart
+    context "when generated" do
+      it "check values" do
+        scale = @svg_chart.scale({
+          :y_label => "Label1",
+          :x_label => "Label2"
+        })
 
-      count_words(line_chart, "<line").should == 6
-      count_words(line_chart, "<text").should == 6
-      count_words(line_chart, "<circle").should == 3
+        scale = scale.gsub( /<[^>]*>/, " ").split
+        scale.should include("Label1")
+        scale.should include("Label2")
+      end
+
+      it "check elements" do
+        scale = @svg_chart.scale({
+          :y_label => "Label1",
+          :x_label => "Label2"
+        })
+
+        count_words(scale, "<line").should == 2
+        count_words(scale, "<text").should == 2
+      end
+    end
+  end
+
+  context "set chart" do
+    before do
+      @svg_chart = SVGCharts::Line.new({
+        :height => 300,
+        :width => 300,
+        :y_retreat => 20,
+        :x_retreat => 20
+      })
     end
 
-    it "check scale elements" do
-      line_chart = SVGCharts::Line.new(@options).draw_scale
+    context "missing parameters" do
+      it "when hash empty" do
+        expect { chart = @svg_chart.draw }.to raise_error(ArgumentError)
+      end
 
-      count_words(line_chart, "<line").should == 2
-      count_words(line_chart, "<text").should == 2
+      it "when the hash keys is incomplete" do
+        options = {
+          :data_color => "#000000",
+          :show_scale => false,
+          :show_dashed => false
+        }
+
+        expect { chart = @svg_chart.draw options }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when generated" do
+      it "check elements with no scale and dashed" do
+        chart = @svg_chart.draw({
+          :scale => ["A", "B", "C"],
+          :data => [10, 20, 30],
+          :data_color => "#000000",
+          :show_scale => false,
+          :show_dashed => false
+        })
+
+        count_words(chart, "<line").should == 3
+        count_words(chart, "<text").should == 3
+        count_words(chart, "<circle").should == 3
+      end
+
+      it "check elements with scale and dashed" do
+        chart = @svg_chart.draw({
+          :scale => ["A", "B", "C"],
+          :data => [10, 20, 30],
+          :data_color => "#000000",
+          :show_scale => true,
+          :show_dashed => true
+        })
+
+        count_words(chart, "<line").should == 6
+        count_words(chart, "<text").should == 6
+        count_words(chart, "<circle").should == 3
+      end
     end
   end
 end
